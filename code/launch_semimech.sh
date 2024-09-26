4d37ff43bf33a9a9d3985198133100c313a5cdbe
Dongah Kim <donga0223@gmail.com>
2024-09-26 12:31:25 -0500

    for submitting

---
+++ code/launch_semimech.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH -J flusion_city                   # Job name
+#SBATCH -o flusion_city.%j.o              # Name of stdout output file (%j expands to jobId)
+#SBATCH -e flusion_city.%j.e              # Name of stderr output file (%j expands to jobId)
+#SBATCH -p normal                        # Queue name, small is for <=2 nodes, normal 3+
+#SBATCH -N 50                  	         # Total number of nodes requested
+#SBATCH -n 2800                          # Total number of tasks to run 56 cores/node (28 per socket)
+#SBATCH -t 48:00:00            	         # Run time (hh:mm:ss)
+#SBATCH -A A-ib1                         # Allocation name
+#SBATCH --mail-user=dongah.kim@utexas.edu   # Email for notifications
+#SBATCH --mail-type=all                  # Type of notifications, begin, end, fail, all
+
+conda activate flusion
+
+# Load launcher
+module load launcher
+
+# Configure launcher
+EXECUTABLE=$TACC_LAUNCHER_DIR/init_launcher
+PRUN=$TACC_LAUNCHER_DIR/paramrun
+CONTROL_FILE=commands_flusion_city_model.txt
+export LAUNCHER_JOB_FILE=commands_flusion_city_model.txt
+export LAUNCHER_WORKDIR=`pwd`
+export LAUNCHER_SCHED=interleaved
+
+# Start launcher
+$PRUN $EXECUTABLE $CONTROL_FILE
